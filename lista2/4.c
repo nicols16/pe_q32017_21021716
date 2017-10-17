b9f1e576e7d0e6a933318b610755865eb85c84aa
unsaferlopes <nicoals.050874@hotmail.com>
2017-10-17 13:08:12 -0200

    Exercícios lista2

--- lista2/4.c
+++ lista2/4.c
@@ -3,43 +3,34 @@
 
 /*Operação soma*/
 
-float soma(int valor){
-        float soma, n1, n2 ;
-        printf("Digite dois números:\n") ;
-        scanf("%f %f", &n1, &n2) ;
-        soma = n1+n2;
-        return soma ;
+float soma(float a, float b)
+{
+        return (a+b) ;
 }
 
 /*Operação subtração*/
 
-float sub(int valor){
-        float sub, n1, n2 ;
-        printf("Digite dois números:\n") ;
-        scanf("%f %f", &n1, &n2) ;
-        sub = n1-n2;
-        return sub ;
+float soma(float a, float b)
+{
+        return (a-b) ;
 }
+
 
 /*Operação multiplicação*/
 
-float mult(int valor){
-        float mult, n1, n2 ;
-        printf("Digite dois números:\n") ;
-        scanf("%f %f", &n1, &n2) ;
-        mult = n1*n2;
-        return mult ;
+float soma(float a, float b)
+{
+        return (a*b) ;
 }
+
 
 /*Operação divisão*/
 
-float div(int valor){
-        float div, n1, n2 ;
-        printf("Digite dois números:\n") ;
-        scanf("%f %f", &n1, &n2) ;
-        div = n1/n2;
-        return div ;
+float soma(float a, float b)
+{
+        return (a/b) ;
 }
+
 
 /*Operação potenciação*/
 
@@ -51,38 +42,48 @@ float pot(int valor){
         return pot ;
 }
 
-/*Função principal*/
+/*Seleção de opção*/
 
-float main(){
-        int valor ;
-        float resp ;
-        printf("Selecione a operação: (1)soma,(2)sub,(3)mult,(4)div ou (5)pot:\n") ;
-        scanf("%d", &valor) ;
+int opcao (int valor)
+{
 
-        if(valor==1){
+if (valor==1) {
         resp = soma(valor) ;
         printf("A resposta é:%f\n", resp) ;
         }
 
-        if(valor==2){
+        if (valor==2) {
         resp = sub(valor) ;
         printf("A resposta é:%f\n", resp) ;
         }
 
-        if(valor==3){
+        if (valor==3) {
         resp = mult(valor) ;
         printf("A resposta é:%f\n", resp) ;
         }
 
-        if(valor==4){
+        if (valor==4) {
         resp = div(valor) ;
         printf("A resposta é:%f\n", resp) ;
         }
 
-        if(valor==5){
+        if (valor==5) {
         resp = pot(valor) ;
-        printf("A resposta é:%f\n", resp) ;
+        printf("%f\n", resp) ;
         }
+}
+
+
+/*Função principal*/
+
+int main(){
+        int valor ;
+        float resp ;
+        printf("Selecione a operação: (1)soma,(2)sub,(3)mult,(4)div ou (5)pot:\n") ;
+        scanf("%d", &valor) ;
+        resp = opcao (valor) ;
+
+       
 }
 
 
