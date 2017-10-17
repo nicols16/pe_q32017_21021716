14fcc4aa1b486673587433aed620974edc1609b4
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-17 00:54:27 +0000

    Exercícios lista2

---
+++ lista2/3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+/*Código de  Gauss-Legendre*/
+
+float GL(int a, float b, float t, int p){
+        float a1, pi ;
+        int cont ;
+
+        while (cont<=10){
+
+        a1 = (a+b)/2 ;
+        b = sqrt((a*b)) ;
+        t = t-p*pow((a-a1),2) ;
+        p = 2*p ;
+        a = a1 ;
+        ++cont ;
+
+        } 
+
+        pi = pow((a+b),2)/(4*t) ;
+        return pi ;
+}
+
+/*Função principal*/
+
+float main(){
+        int a = 1, p = 1 ;
+        float b = 1/sqrt(2), t = 1/4 ;
+        float pi ;
+       
+        pi = GL(a, b, p, t) ;
+        printf("%.20f\n",&pi) ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
