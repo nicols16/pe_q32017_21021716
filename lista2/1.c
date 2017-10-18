a79cbef63cccc14347b9b478e8a370489ec0459d
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-18 02:44:07 +0000

    Exercícios lista2

--- lista2/1.c
+++ lista2/1.c
@@ -2,10 +2,10 @@
 
 /*Confere se o número é par ou ímpar*/
 
-int main(){
+int main()
+{
         int x ;
 
-        printf("Digite um numero:\n") ;
         scanf("%d", &x) ;
         if(x%2!=0) printf("%d\n", 1) ;
         if(x%2==0) printf("%d\n", 0) ;
