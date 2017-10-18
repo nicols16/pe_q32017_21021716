8c4faceb3d44a50f38f861ae439d14fe459d5bfd
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-18 03:33:46 +0000

    Exercícios lista2

---
+++ lista2/5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+/*Função que calcula o fatorial*/
+
+int fat(int a)
+{
+        
+        int fator = a ;
+
+        for(int i=(a-1);i!=0;--i){
+          fator = fator*i ;
+        }
+
+        return fator ; 
+         
+}
+
+/*Função que calcula o coeficiente*/
+
+int coef(int fatorialn, int fatorialk, int fatorialnk)
+{
+        return (fatorialn)/((fatorialk)*(fatorialnk)) ;
+}
+
+/*Funçãi principal*/
+
+int main()
+{
+
+        int n, k, cb ;
+        int fatorialn, fatorialk, fatorialnk ;
+       
+        scanf("%d %d", &n, &k) ;
+        fatorialnk = fat(n-k) ; 
+        fatorialn = fat(n) ;
+        fatorialk = fat(k) ;
+        cb = coef(fatorialn, fatorialk, fatorialnk) ;
+        printf("%d\n", cb) ;
+        return 0 ;
+}
+        
+        
+
+
+
+
+
