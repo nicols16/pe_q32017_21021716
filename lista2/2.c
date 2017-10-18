399de86e2601413a8411ba5987ba9621a9085d6d
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-18 02:39:59 +0000

    Exercícios lista2

--- lista2/2.c
+++ lista2/2.c
@@ -2,20 +2,19 @@
 
 /*Função que calcula o volume em ponto flutuante*/
 
-float volume(float h, float l, float c){
-        float vol ;
-        vol = l*h*c ;
-        return vol ;
+float volume(float h, float l, float c)
+{
+       
+        return l*h*c ;
 }
 
 /*Função principal*/
 
-float main(){
+int main()
+{
         float l, h, c ;
         float resultado ;
-
-        printf("Determine os valores da altura, largura e comprimento: \n") ;
         scanf("%f %f %f", &h, &l, &c) ;
         resultado = volume(h, l, c) ;
-        printf("O vlume é:%.2f\n", resultado) ;
+        printf("%.2f\n", resultado) ;
 }
