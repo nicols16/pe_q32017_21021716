39e3213c22119008cf9a75696da036b32cfebce4
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-20 03:43:50 +0000

    Exercícios lista2

--- lista2/5.c
+++ lista2/5.c
@@ -18,25 +18,40 @@ int fat(int a)
 
 /*Função que calcula o coeficiente*/
 
-int coef(int fatorialn, int fatorialk, int fatorialnk)
+void coef(int fatorialn, int fatorialk, int fatorialnk)
 {
-        return (fatorialn)/((fatorialk)*(fatorialnk)) ;
+
+        int cb ;
+
+        cb = (fatorialn)/((fatorialk)*(fatorialnk)) ;
+        printf("%d\n", cb) ;
 }
+
+/*Função que seleciona o fatorial a ser calculado
+* e repassa os valores para o cálculo do coeficiente*/
+
+void selecao(int n, int k)
+{
+
+        int fatorialn, fatorialk, fatorialnk ;
+        
+        fatorialnk = fat(n-k) ; 
+        fatorialn = fat(n) ;
+        fatorialk = fat(k) ;
+        coef(fatorialn, fatorialk, fatorialnk) ;
+
+
 
 /*Função principal*/
 
 int main()
 {
 
-        int n, k, cb ;
+        int n, k, cb, sel;
         int fatorialn, fatorialk, fatorialnk ;
        
         scanf("%d %d", &n, &k) ;
-        fatorialnk = fat(n-k) ; 
-        fatorialn = fat(n) ;
-        fatorialk = fat(k) ;
-        cb = coef(fatorialn, fatorialk, fatorialnk) ;
-        printf("%d\n", cb) ;
+        sel = selecao(n, k) ;
         return 0 ;
 }
         
