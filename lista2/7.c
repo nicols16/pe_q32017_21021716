4215c69bab5bea60c548922a4eed0dbfd6a72927
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-20 19:30:25 +0000

    Exercícios lista2

--- lista2/7.c
+++ lista2/7.c
@@ -11,42 +11,42 @@ void temp(int sel2, int sel3, float valor)
 
         float t ;
 
-        if(sel2==1&&sel3==2){
+        if (sel2==1&&sel3==2) {
 
          t = valor*1.8+32 ;
          printf("%.2fF\n", t) ;
    
         } 
 
-        else if(sel2==1&&sel3==3){
+        else if (sel2==1&&sel3==3) {
 
          t = valor + 273.15 ;
          printf("%.2fK\n", t) ;
 
         }
 
-        else if(sel2==2&&sel3==1){
+        else if (sel2==2&&sel3==1) {
 
          t = (valor-32)/1.8 ;
          printf("%.2fC\n", t) ;
 
         }
 
-        else if(sel2==2&&sel3==3){
+        else if (sel2==2&&sel3==3) {
 
          t = 5*((valor-32)/9)+273.15 ;
          printf("%.2fK\n", t) ;
 
         }
 
-        else if(sel2==3&&sel3==1){
+        else if (sel2==3&&sel3==1) {
 
          t = valor-273.15 ;
          printf("%.2fC\n", t) ;
 
         }
 
-        else{
+        else {
 
          t = 9*((valor-273.15)/5)+32 ;
          printf("%.2fF\n", t) ;
@@ -64,19 +64,19 @@ void bnum(int sel2, int sel3, float valor)
 
         int num ;
 
-        if(sel2==1){
+        if (sel2==1) {
 
-          for(int i = 31;i>=0;i--){
+          for (int i = 31;i>=0;i--) {
 
           num = (int) valor >> i ;
 
-            if(num&1){
+            if (num&1) {
 
             printf("1") ;
 
             }
 
-            else{
+            else {
 
             printf("0") ;
 
@@ -89,11 +89,11 @@ void bnum(int sel2, int sel3, float valor)
         
         }
 
-         else{
+         else {
 
          num = 0 ;
 
-         for(int i = 0; valor > 0; i++){
+         for (int i = 0; valor > 0; i++) {
 
          num = num + pow(2, i) * ((int) valor % 10);
          valor = valor / 10;
@@ -116,13 +116,13 @@ void bnum(int sel2, int sel3, float valor)
 void tipos(int sel1, int sel2, int sel3, float valor)
 {
 
-        if(sel1==1){
+        if (sel1==1) {
 
          temp(sel2, sel3, valor) ;
 
         }
 
-         else{
+         else {
          bnum(sel2, sel3, valor) ;
 
          }
