4dcd4d6a27fa0cc88242b2a921a6fc196b1c3e1f
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-20 20:54:30 +0000

    Exercícios lista2

---
+++ lista2/9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int pot(int x, int y)
+{
+     
+        if (y==0) return 1 ;
+        return (pot(x, (y-1))*x) ;
+
+}
+
+int main()
+{
+
+        int x, y ;
+
+        scanf("%d %d\n", &x, &y) ;
+        printf("%d\n", pot(x, (y-1))*x) ;
+        return 0 ;
+
+}
