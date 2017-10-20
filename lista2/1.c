971ea5fe9712afbfa6b0fda7f0390eccc068213d
unsaferlopes <nicolas.050874@hotmail.com>
2017-10-20 19:24:56 +0000

    Exercícios lista2

--- lista2/1.c
+++ lista2/1.c
@@ -7,8 +7,8 @@ int main()
         int x ;
 
         scanf("%d", &x) ;
-        if(x%2!=0) printf("%d\n", 1) ;
-        if(x%2==0) printf("%d\n", 0) ;
+        if (x%2!=0) printf("%d\n", 1) ;
+        if (x%2==0) printf("%d\n", 0) ;
         
         return 0 ;
 }
