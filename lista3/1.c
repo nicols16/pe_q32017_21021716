cde0bb0576ac3248167a3cae38b4fe526c70ee11
unsaferlopes <nicolas.050874@hotmail.com>
2017-11-07 00:33:26 +0000

    Exercícios lista3

--- lista3/1.c
+++ lista3/1.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-/*Função que recebe o vetor e conta as vogais*/
 
-int vogais(char *pv,  int n)
+int vogal (char c)
 {
-
-        int cont = 0;
-
-        for (int i = 0; i<n; i++) {
-
-         if (pv[i] == 'a'||'e'||'i'||'o'||'u') {
-
-         cont +=1;
-         }
-
-        }
-
-        printf("%d\n", cont);
+    if ((c == 'A')||(c == 'E')||(c == 'I')||(c == 'O')||(c == 'U')||(c == 'a')||(c == 'e')||(c == 'i')||(c == 'o')||(c == 'u')) {
+        return 1;
+    }
+    return 0;
 }
 
-/*Função principal*/
+
+int n_vogais(char * entrada)
+{
+    int i, n=0;
+    for (i=0; i<255; i++) {
+        if (*(entrada+i) == '\0') break;
+        if (vogal(*(entrada+i))) {
+            n++;
+        }
+    }
+    return n;
+}
 
 int main()
 {
+    char entrada [255];
+    fgets (entrada, 255, stdin);
 
-        char v[255];
-        char *pv = v;
-        int n = 255;
+    printf("%d\n", n_vogais(entrada));
 
-        printf("Escreva uma palavra:\n ");
-        fgets(v, 255, stdin);
-        vogais(pv,n); 
-        return 0;
+    return 0;
 }
