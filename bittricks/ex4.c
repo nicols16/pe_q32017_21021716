8bd8bee623303b4bfc96864843bfcfaff0639d8c
gpeterlini <peterlini88@gmail.com>
2017-11-17 11:17:37 -0200

    Lab bit-tricks

---
+++ bittricks/ex4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define N 1000000000
+
+unsigned int F(unsigned int x, unsigned int y)
+{
+
+        return x<<y;
+}
+
+int main()
+{
+
+        unsigned int i, soma = 0;
+    
+        for (i = 0; i<N; i++) {
+         soma += F(i,1);
+        }
+
+        printf("%u\n", soma);
+        return 0;
+}
